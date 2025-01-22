ec990fb57d9ffa01d0fca781f57a306a50301750
Yoppo40 <158439801+Yoppo40@users.noreply.github.com>
2025-01-22 11:38:23 +0900

    Update shisakustreamlit.py

--- shisakustreamlit.py
+++ shisakustreamlit.py
@@ -234,11 +234,7 @@ for column in df_numeric.columns:
     else:
         st.altair_chart(chart)
 
-import time
-
 # 自動更新の処理
-if auto_update:  # 自動更新がオンになっているとき
-    st.write("リアルタイム更新モードが有効です。")
-    time.sleep(update_interval)  # 指定した秒数だけ待つ
-    st.experimental_rerun()  # プログラムを最初からやり直す
-
+if auto_update:
+    time.sleep(update_interval)
+    st.experimental_rerun()
